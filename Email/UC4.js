c991b56ce0df4b8e4dd5a4e0c3c63822f4e4a647
manjugoutham <gouthamy219@gmail.com>
2022-01-28 12:40:17 +0530

    Objects and classes practice problems

--- Email/UC4.js
+++ Email/UC4.js
@@ -1,11 +1,11 @@
 
 function emailcom(valid,invalid)
 {
-let pattern = RegExp("([a-zA-Z0-9]{3,}|[a-zA-Z0-9]{3,}[+_.-]?[a-zA-Z0-9]{1,})[@]{1}[g][m][a][i][l][.][c][o][m]");
+let pattern = RegExp("([a-zA-Z0-9]{3,}|[a-zA-Z0-9]{3,}[+_.-]?[a-zA-Z0-9]{1,})[@]{1}[b][r][i][d][g][e][l][a][b][z][.][c][o][m]");
 
     let validemail = pattern.test(valid);
     let invalidemail = pattern.test(invalid);
     console.log(validemail);      
     console.log(invalidemail);  
 }
-emailcom("bridge+labz@gmail.com","bridgelabz@gmail+com");
+emailcom("bridgelabz+abc@gmail.com","bridgelabz+abc@bridgelabz.com");
